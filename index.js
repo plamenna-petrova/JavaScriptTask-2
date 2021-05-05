5896f14a9be1a23d3e6ea4a46db564411f82a847
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-05-05 18:02:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+
+// add a new field when enter is being pressed
+
+document.addEventListener("keydown", addNewInputWithEnter);
+
+function addNewInputWithEnter(event) {
+    if (event.keyCode === 13) {
+        var textInput = document.createElement('input');
+        textInput.setAttribute('type', 'text');
+
+        var quantity = document.createElement('input');
+        quantity.setAttribute('type', 'number');
+        quantity.addEventListener('keyup', calculateTotal);
+
+        var price = document.createElement('input');
+        price.setAttribute('type', 'number');
+        price.addEventListener('keyup', calculateTotal);
+
+        var total = document.createElement('span');
+
+        var buttonDelete = document.createElement('button');
+        buttonDelete.innerText = "X";
+        buttonDelete.addEventListener('click', deleteRow);
+
+        var div = document.createElement('div');
+        div.appendChild(textInput);
+        div.appendChild(quantity);
+        div.appendChild(price);
+        div.appendChild(total);
+        div.appendChild(buttonDelete);
+
+        var section = document.querySelector('section');
+        section.appendChild(div);
+    }
+}
+
+// add new fields via Add button
+
+// var buttonAdd = document.querySelector(".add");
+// buttonAdd.addEventListener('click', addNewInput);
+
+// // addNewInput();
+
+// function addNewInput() {
+//     var textInput = document.createElement('input');
+//     textInput.setAttribute('type', 'text');
+
+//     var quantity = document.createElement('input');
+//     quantity.setAttribute('type', 'number');
+//     quantity.addEventListener('keyup', calculateTotal);
+
+//     var price = document.createElement('input');
+//     price.setAttribute('type', 'number');
+//     price.addEventListener('keyup', calculateTotal);
+
+//     var total = document.createElement('span');
+
+//     var buttonDelete = document.createElement('button');
+//     buttonDelete.innerText = "X";
+//     buttonDelete.addEventListener('click', deleteRow);
+
+//     var div = document.createElement('div');
+//     div.appendChild(textInput);
+//     div.appendChild(quantity);
+//     div.appendChild(price);
+//     div.appendChild(total);
+//     div.appendChild(buttonDelete);
+
+//     var section = document.querySelector('section');
+//     section.appendChild(div);
+// }
+
+function calculateTotal(event) {
+    var div = event.target.parentNode;
+    console.log(div);
+    var quantity = div.childNodes[1];
+    var price = div.childNodes[2];
+    var total = div.childNodes[3];
+
+    console.log(quantity.value);
+    if (isNaN(quantity.value)) {
+        quantity.value = 0;
+    }
+
+    if (isNaN(price.value)) {
+        price.value = 0;
+    }
+
+    total.innerText = price.value * quantity.value;
+}
+
+function deleteRow(event) {
+    event.target.parentNode.remove();
+}
